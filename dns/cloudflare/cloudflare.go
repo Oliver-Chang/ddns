291c9122827d878a073a14f08f779b8c71aecf70
Oliver-Chang <oliverchangnt@gmail.com>
2019-02-20 14:34:49 +0800

    测试

--- dns/cloudflare/cloudflare.go
+++ dns/cloudflare/cloudflare.go
@@ -3,71 +3,110 @@ package cloudflare
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/Oliver-Chang/ddns/util/logger"
+
+	"github.com/pkg/errors"
 )
 
-const newRecordAPI = "https://api.cloudflare.com/client/v4/zones/%s/dns_records"
+const apiURL = "https://api.cloudflare.com/client/v4"
 
 // CloudFlare CloudFlare
 type CloudFlare struct {
-	authID  string
-	zoneID  string
-	authKey string
+	AuthEmail string
+	AuthKey   string
+	baseURL   string
+	headers   *http.Header
+	client    *http.Client
 }
 
 // New New
-func New(authID, zoneID, authKey string) *CloudFlare {
+func New(authEmail, authKey string) *CloudFlare {
+	headers := make(http.Header)
+	headers.Add("X-Auth-Email", authEmail)
+	headers.Add("X-Auth-Key", authKey)
+	headers.Add("Content-Type", "application/json")
+	client := http.DefaultClient
 	return &CloudFlare{
-		authID:  authID,
-		zoneID:  zoneID,
-		authKey: authKey,
+		AuthEmail: authEmail,
+		AuthKey:   authKey,
+		headers:   &headers,
+		client:    client,
+		baseURL:   apiURL,
 	}
+
+}
+
+// Record Record
+type Record struct {
+	ID         string      `json:"id,omitempty"`
+	Type       string      `json:"type,omitempty"`
+	Name       string      `json:"name,omitempty"`
+	Content    string      `json:"content,omitempty"`
+	Proxiable  bool        `json:"proxiable,omitempty"`
+	Proxied    bool        `json:"proxied"`
+	TTL        int         `json:"ttl,omitempty"`
+	Locked     bool        `json:"locked,omitempty"`
+	ZoneID     string      `json:"zone_id,omitempty"`
+	ZoneName   string      `json:"zone_name,omitempty"`
+	CreatedOn  time.Time   `json:"created_on,omitempty"`
+	ModifiedOn time.Time   `json:"modified_on,omitempty"`
+	Data       interface{} `json:"data,omitempty"` // data returned by: SRV, LOC
+	Meta       interface{} `json:"meta,omitempty"`
+	Priority   int         `json:"priority"`
 }
 
 // CreateRecord CreateRecord
-func (c *CloudFlare) CreateRecord(ipv6, domain string) error {
+func (c *CloudFlare) CreateRecord(zoneID string, record Record) error {
 	var (
-		req  *http.Request
-		resp *http.Response
-		data map[string]string
-		err  error
-		url  string
-		body []byte
+		res []byte
+		err error
 	)
-	data = map[string]string{
-		"type":    "AAAA",
-		"name":    domain,
-		"content": ipv6,
-	}
-	bytesJSON, err := json.Marshal(data)
+	url := c.baseURL + "/zones/" + zoneID + "/dns_records"
+	res, err = c.makeRequest("POST", url, record)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
-	fmt.Println(string(bytesJSON))
-	url = fmt.Sprintf(newRecordAPI, c.zoneID)
-	req, err = http.NewRequest("POST", url, bytes.NewBuffer(bytesJSON))
-	if err != nil {
-		fmt.Println(err)
-		return err
+	logger.Logger.WithField("res", res)
+	return nil
+}
+
+func (c *CloudFlare) makeRequest(method, url string, params interface{}) ([]byte, error) {
+	var (
+		err       error
+		jsonBtyes []byte
+		reqBody   io.Reader
+	)
+	if params != nil {
+		if paramsBytes, ok := params.([]byte); ok {
+			jsonBtyes = paramsBytes
+		} else {
+			jsonBtyes, err = json.Marshal(params)
+			if err != nil {
+				return nil, errors.Wrap(err, "Can't marshal params to bytes")
+			}
+		}
+	} else {
+		jsonBtyes = nil
 	}
-	req.Header.Add("X-Auth-Email", c.authID)
-	req.Header.Add("X-Auth-Key", c.authKey)
-	req.Header.Add("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err = client.Do(req)
+	reqBody = bytes.NewReader(jsonBtyes)
+	req, err := http.NewRequest(method, url, reqBody)
 	if err != nil {
-		fmt.Println(err)
-		return err
+		return nil, errors.Wrap(err, "Create Request failed")
+	}
+	req.Header = *c.headers
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "Request failed")
 	}
 	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
+	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
-		return err
+		return nil, errors.Wrap(err, "Read respone body failed")
 	}
-	fmt.Println(string(body))
-	return nil
+	return respBody, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/errors v0.8.1
 	github.com/robfig/cron v0.0.0-20180505203441-b41be1df6967
 	github.com/sirupsen/logrus v1.3.0
 	github.com/spf13/cobra v0.0.3
